a1f2a58a3fd5a39be18d88668cb0d0afb8d6965f
Rohxn14 <146233006+Rohxn14@users.noreply.github.com>
2023-10-10 12:54:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const stadiums = [
     {
         name: "Camp Nou",
         image: "Images/camp.jpg" ,
-        description: "The Camp Nou is another iconic football (soccer) stadium, located in Barcelona, Spain. It is the home stadium of FC Barcelona, one of the most successful and popular football clubs in the world.",
+        description: "Camp Nou, situated in Barcelona, Spain, is one of the world's most iconic football stadiums and the home ground of FC Barcelona. With a seating capacity of over 99,000 fans, it's Europe's largest stadium. Camp Nou has witnessed countless historic matches and unforgettable moments, hosting both domestic and international competitions. Its stunning architecture and the passionate 'Culés' fanbase create an electrifying atmosphere.",
         team: "FC Barcelona",
         link: "https://www.google.com/search?q=FC+Barcelona"
     },
@@ -31,7 +31,7 @@ const stadiums = [
     },
     {
         name: "Allianz Arena",
-        image: "Images/allianz-arena.webp",
+        image: "Images/allianz-arena.webp" ,
         description: "The Allianz Arena, located in Munich, Germany, is a world-renowned football stadium. Home to FC Bayern Munich and TSV 1860 Munich, its distinctive illuminated façade changes colors to match the home team's kit.",
         team:"Bayern Munich",
         link: "https://www.google.com/search?q=Bayern+munich"
@@ -57,48 +57,122 @@ const stadiums = [
         team: "Borrusia Dortmund",
         link: "https://www.google.com/search?q=borussia+dortmund"
     },
+    {
+        name: "Stadio Olimpico",
+        image: "Images/roma.jpg",
+        description: "Stadio Olimpico, located in Rome, Italy, is a historic multi-purpose stadium renowned for its significant role in Italian sports and culture. With a seating capacity exceeding 70,000 spectators, it has hosted numerous memorable events, including the 1960 Summer Olympics and the 1990 FIFA World Cup. Home to both A.S. Roma and S.S. Lazio football clubs, the stadium is at the heart of the city's passionate football rivalry.",
+        team: "A.S Roma",
+        link: "https://www.google.com/search?q=as+roma"
+    },
     
     // Add data for other stadiums similarly
 ];
 
-// Function to generate stadium cards and append them to the stadium container
-function generateStadiumCards() {
+function generateStadiumCard(stadium) {
+    const stadiumCard = document.createElement("div");
+    stadiumCard.classList.add("stadium");
+  
+    const stadiumImageLink = document.createElement("a");
+    stadiumImageLink.href = stadium.link;
+    stadiumImageLink.target = "_blank";
+  
+    const stadiumImage = document.createElement("img");
+    stadiumImage.src = stadium.image;
+    stadiumImage.alt = stadium.name;
+  
+    const stadiumTitle = document.createElement("h2");
+    stadiumTitle.textContent = stadium.name;
+  
+    const stadiumDescription = document.createElement("p");
+    stadiumDescription.textContent = stadium.description;
+  
+    const stadiumTeam = document.createElement("p");
+    stadiumTeam.textContent = "Home Team: " + stadium.team;
+  
+    stadiumImageLink.appendChild(stadiumImage);
+    stadiumCard.appendChild(stadiumImageLink);
+    stadiumCard.appendChild(stadiumTitle);
+    stadiumCard.appendChild(stadiumDescription);
+    stadiumCard.appendChild(stadiumTeam);
+  
+    return stadiumCard;
+  }
+  
+  // Function to generate stadium cards and append them to the stadium container
+  function generateStadiumCards() {
     const stadiumContainer = document.querySelector(".stadium-container");
-
+  
     stadiums.forEach((stadium, index) => {
-        const stadiumCard = document.createElement("div");
-        stadiumCard.classList.add("stadium");
-
-        const stadiumImageLink = document.createElement("a"); // Create a link for the image
-        stadiumImageLink.href = stadium.link;
-        stadiumImageLink.target = "_blank";
-
-        const stadiumImage = document.createElement("img");
-        stadiumImage.src = stadium.image;
-        stadiumImage.alt = stadium.name;
-
-        const stadiumTitle = document.createElement("h2");
-        stadiumTitle.textContent = stadium.name;
-
-        const stadiumDescription = document.createElement("p");
-        stadiumDescription.textContent = stadium.description;
-
-        const stadiumTeam = document.createElement("p");
-        stadiumTeam.textContent = "Home Team: " + stadium.team;
-
-        stadiumImageLink.appendChild(stadiumImage); // Wrap the image in the link
-
-        stadiumCard.appendChild(stadiumImageLink); // Append the link to the card
-        stadiumCard.appendChild(stadiumTitle);
-        stadiumCard.appendChild(stadiumDescription);
-        stadiumCard.appendChild(stadiumTeam);
-
-        stadiumContainer.appendChild(stadiumCard);
+      const stadiumCard = generateStadiumCard(stadium);
+      stadiumContainer.appendChild(stadiumCard);
     });
+  }
+  
+  // Function to handle the image slider
+  function handleImageSlider() {
+    let slideIndex = 0;
+    showSlide();
+  
+    function showSlide() {
+      const slides = document.querySelectorAll(".myslides");
+      const dots = document.querySelectorAll(".dot");
+  
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+  
+      slideIndex++;
+  
+      if (slideIndex > slides.length) {
+        slideIndex = 1;
+      }
+  
+      for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+      }
+  
+      slides[slideIndex - 1].style.display = "block";
+      dots[slideIndex - 1].classList.add("active");
+  
+      setTimeout(showSlide, 2000); // Change image every 4 seconds
+    }
+  }
+  
+  // Call the functions to generate stadium cards and start the image slider
+  generateStadiumCards();
+  handleImageSlider();
+
+  const leftArrow = document.querySelector(".left-arrow");
+const rightArrow = document.querySelector(".right-arrow");
+let slideIndex = 0;
+
+leftArrow.addEventListener("click", () => {
+    showSlide(slideIndex - 1);
+});
+
+rightArrow.addEventListener("click", () => {
+    showSlide(slideIndex + 1);
+});
+
+function showSlide(index) {
+    const slides = document.querySelectorAll(".myslides");
+
+    if (index < 0) {
+        slideIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        slideIndex = 0;
+    } else {
+        slideIndex = index;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
 }
 
-// Call the function to generate stadium cards
-generateStadiumCards();
-
+// Call showSlide to display the initial slide
+showSlide(slideIndex);
 
 
